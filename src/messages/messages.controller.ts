04e1065cc72d896f3ad8dc328d990044ece2f9c9
hozali <hozan949@gmail.com>
2023-04-27 23:52:53 +0100

    Add Messages Service to the controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,7 +1,15 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    //this will be removed
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
   listMessages() {}
 
